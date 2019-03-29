707373dad9a967c83bad75ae4149cb8f126d66a7
Dwitry <dwitry@users.noreply.github.com>
2019-03-29 16:17:58 +0200

    Contribute Cypher for Gremlin TCK reporting
    Signed-off-by: Dwitry dwitry@users.noreply.github.com

---
+++ tools/tck-reporting/src/main/java/org/opencypher/tools/tck/reporting/SystemOutReader.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2015-2019 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Attribution Notice under the terms of the Apache License 2.0
+ *
+ * This work was created by the collective efforts of the openCypher community.
+ * Without limiting the terms of Section 6, any Derivative Work that is not
+ * approved by the public consensus process of the openCypher Implementers Group
+ * should not be described as “Cypher” (and Cypher® is a registered trademark of
+ * Neo4j Inc.) or as "openCypher". Extensions by implementers or prototypes or
+ * proposals for change that have been documented or implemented should only be
+ * described as "implementation extensions to Cypher" or as "proposed changes to
+ * Cypher that are not yet approved by the openCypher community".
+ */
+package org.opencypher.tools.tck.reporting;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+class SystemOutReader {
+    private PrintStream systemOut = System.out;
+    private ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+    private TeeOutputStream teeOutput = new TeeOutputStream(System.out, buffer);
+
+    SystemOutReader() {
+        System.setOut(new PrintStream(teeOutput));
+    }
+
+    void close() throws IOException {
+        System.setOut(systemOut);
+        buffer.close();
+    }
+
+    String clear() {
+        String output = buffer.toString();
+        buffer.reset();
+        return output;
+    }
+
+    static class TeeOutputStream extends OutputStream {
+        private OutputStream out1;
+        private OutputStream out2;
+
+        TeeOutputStream(final OutputStream out1, final OutputStream out2) {
+            this.out1 = out1;
+            this.out2 = out2;
+        }
+
+        @Override
+        public void write(final byte[] b) throws IOException {
+            this.out1.write(b);
+            this.out2.write(b);
+        }
+
+        @Override
+        public void write(final byte[] b, final int off, final int len) throws IOException {
+            this.out1.write(b, off, len);
+            this.out2.write(b, off, len);
+        }
+
+        @Override
+        public void write(final int b) throws IOException {
+            this.out1.write(b);
+            this.out2.write(b);
+        }
+
+        @Override
+        public void flush() throws IOException {
+            this.out1.flush();
+            this.out2.flush();
+        }
+
+        @Override
+        public void close() throws IOException {
+            try {
+                this.out1.close();
+            } finally {
+                this.out2.close();
+            }
+        }
+
+    }
+
+}
---
+++ tools/tck-reporting/src/test/java/org/opencypher/tools/tck/reporting/CucumberReportAdapterTest.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2015-2019 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Attribution Notice under the terms of the Apache License 2.0
+ *
+ * This work was created by the collective efforts of the openCypher community.
+ * Without limiting the terms of Section 6, any Derivative Work that is not
+ * approved by the public consensus process of the openCypher Implementers Group
+ * should not be described as “Cypher” (and Cypher® is a registered trademark of
+ * Neo4j Inc.) or as "openCypher". Extensions by implementers or prototypes or
+ * proposals for change that have been documented or implemented should only be
+ * described as "implementation extensions to Cypher" or as "proposed changes to
+ * Cypher that are not yet approved by the openCypher community".
+ */
+package org.opencypher.tools.tck.reporting;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.opencypher.tools.tck.api.CypherTCK;
+import org.opencypher.tools.tck.api.CypherValueRecords;
+import org.opencypher.tools.tck.api.ExecutionFailed;
+import org.opencypher.tools.tck.api.Graph;
+import org.opencypher.tools.tck.api.QueryType;
+import org.opencypher.tools.tck.api.Scenario;
+import org.opencypher.tools.tck.api.StringRecords;
+import org.opencypher.tools.tck.values.CypherValue;
+import scala.collection.JavaConverters;
+import scala.collection.Seq;
+import scala.collection.immutable.List;
+import scala.collection.immutable.Map;
+import scala.collection.immutable.Set;
+import scala.runtime.AbstractFunction0;
+import scala.util.Either;
+import scala.util.Left;
+import scala.util.Right;
+
+@ExtendWith({ReportValidator.class, CucumberReportAdapter.class})
+public class CucumberReportAdapterTest {
+    @TestFactory
+    public Stream<DynamicTest> runFeatures() throws Exception {
+        String content = getResource("Foo.feature");
+
+        Seq<Scenario> scenariosSeq = CypherTCK.parseFeature("Foo.feature", content).scenarios();
+        java.util.List<Scenario> scenarios = JavaConverters.seqAsJavaList(scenariosSeq);
+
+        AbstractFunction0<Graph> graph = graph();
+
+        return scenarios.stream()
+            .map(scenario -> {
+                String name = scenario.toString();
+                Executable executable = scenario.apply(graph);
+                return DynamicTest.dynamicTest(name, executable);
+            });
+    }
+
+    private class FakeGraph implements Graph {
+        @Override
+        public Either<ExecutionFailed, CypherValueRecords> cypher(String query, Map<String, CypherValue> params, QueryType meta) {
+            if (query.contains("foo()")) {
+                return new Left<>(new ExecutionFailed("SyntaxError", "compile time", "UnknownFunction", null));
+            } else if (query.startsWith("RETURN ")) {
+                String result = query.replace("RETURN ", "");
+                System.out.println("Producing some output " + result);
+                List<String> header = new Set.Set1<>(result).toList();
+                Map<String, String> row = new Map.Map1<>(result, result);
+                List<Map<String, String>> rows = new Set.Set1<>(row).toList();
+                return new Right<>(new StringRecords(header, rows).asValueRecords());
+            } else {
+                return new Right<>(CypherValueRecords.empty());
+            }
+        }
+    }
+
+    private AbstractFunction0<Graph> graph() {
+        FakeGraph fakeGraph = new FakeGraph();
+        return new AbstractFunction0<Graph>() {
+            @Override
+            public Graph apply() {
+                return fakeGraph;
+            }
+        };
+    }
+
+    static String getResource(String path) throws IOException {
+        InputStream resource = CucumberReportAdapterTest.class.getResourceAsStream(path);
+        if (resource == null) {
+            throw new IOException(path + " not found");
+        }
+        return new Scanner(resource, "UTF-8").useDelimiter("\\A").next();
+    }
+}
