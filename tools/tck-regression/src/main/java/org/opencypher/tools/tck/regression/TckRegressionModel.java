b381d2d5371c006c051d13ce5b1eb889b832b9b6
Dwitry <dwitry@users.noreply.github.com>
2019-05-15 16:46:06 +0300

    Contribute Cypher for Gremlin TCK regression
    See README.adoc
    
    Signed-off-by: Dwitry dwitry@users.noreply.github.com

---
+++ tools/tck-regression/src/main/java/org/opencypher/tools/tck/regression/TckRegressionModel.java
@@ -0,0 +1,233 @@
+/*
+ * Copyright (c) 2015-2019 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Attribution Notice under the terms of the Apache License 2.0
+ *
+ * This work was created by the collective efforts of the openCypher community.
+ * Without limiting the terms of Section 6, any Derivative Work that is not
+ * approved by the public consensus process of the openCypher Implementers Group
+ * should not be described as “Cypher” (and Cypher® is a registered trademark of
+ * Neo4j Inc.) or as "openCypher". Extensions by implementers or prototypes or
+ * proposals for change that have been documented or implemented should only be
+ * described as "implementation extensions to Cypher" or as "proposed changes to
+ * Cypher that are not yet approved by the openCypher community".
+ */
+package org.opencypher.tools.tck.regression;
+
+import static java.lang.String.format;
+import static java.util.Collections.unmodifiableList;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.toList;
+import static org.opencypher.tools.tck.regression.TckResultsComparator.checkState;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class TckRegressionModel {
+    static class Scenarios {
+        private final List<Scenario> all;
+        private final List<Scenario> passing;
+        private final List<Scenario> failed;
+
+        private Scenarios(List<Scenario> all, List<Scenario> passing, List<Scenario> failed) {
+            this.all = all;
+            this.passing = passing;
+            this.failed = failed;
+        }
+
+        static Scenarios create(TestSuite feature) {
+            List<Scenario> all = unmodifiableList(feature.testcase);
+
+            List<Scenario> passed = all.stream()
+                .filter(Scenario::isPassed)
+                .collect(collectingAndThen(toList(), Collections::unmodifiableList));
+
+            List<Scenario> failed = all.stream()
+                .filter(Scenario::isFailed)
+                .collect(collectingAndThen(toList(), Collections::unmodifiableList));
+
+            return new Scenarios(all, passed, failed);
+        }
+
+        List<Scenario> all() {
+            return all;
+        }
+
+        Diff compare(Scenarios other) {
+            if (this.all.size() != other.all.size()) {
+                throw new IllegalStateException(format("Can not compare: all size changed from %s to %s",
+                    other.all.size(),
+                    this.all.size()));
+            }
+
+            List<Scenario> newlyPassingScenarios = subtract(this.passing, other.passing);
+            List<Scenario> newlyFailedScenarios = subtract(this.failed, other.failed);
+            int totalPassingScenarios = passing.size();
+            int totalScenarios = all.size();
+
+            return new Diff(newlyPassingScenarios, newlyFailedScenarios,
+                totalPassingScenarios, totalScenarios, all);
+        }
+
+        Diff verify(Scenarios other) {
+            List<Scenario> newlyPassingScenarios = subtract(other.failed, this.failed);
+            List<Scenario> newlyFailedScenarios = subtract(this.failed, other.failed);
+            int totalPassingScenarios = passing.size();
+            int totalScenarios = all.size();
+
+            return new Diff(newlyPassingScenarios, newlyFailedScenarios,
+                totalPassingScenarios, totalScenarios, all);
+        }
+
+        private <T> List<T> subtract(final List<T> list1, final List<T> list2) {
+            final ArrayList<T> result = new ArrayList<>(list1);
+            list2.forEach(result::remove);
+
+            return result;
+        }
+    }
+
+    @SuppressWarnings("unused")
+    public static class Diff {
+        private final List<Scenario> newlyPassingScenarios;
+        private final List<Scenario> newlyFailedScenarios;
+        private final List<Scenario> allScenarios;
+        private final int totalPassingScenarios;
+        private final int totalScenarios;
+
+        String passingPercentage;
+
+        Diff(List<Scenario> newlyPassingScenarios, List<Scenario> newlyFailedScenarios,
+             int totalPassingScenarios, int totalScenarios, List<Scenario> allScenarios) {
+            this.newlyPassingScenarios = unmodifiableList(newlyPassingScenarios);
+            this.newlyFailedScenarios = unmodifiableList(newlyFailedScenarios);
+            this.allScenarios = unmodifiableList(allScenarios);
+            this.totalPassingScenarios = totalPassingScenarios;
+            this.totalScenarios = totalScenarios;
+            this.passingPercentage = new DecimalFormat("#.##").format((0.0 + totalPassingScenarios) / totalScenarios * 100) + "%";
+        }
+
+        public List<Scenario> getNewlyPassingScenarios() {
+            return newlyPassingScenarios;
+        }
+
+        public List<Scenario> getNewlyFailedScenarios() {
+            return newlyFailedScenarios;
+        }
+
+        public int getTotalPassingScenarios() {
+            return totalPassingScenarios;
+        }
+
+        public int getTotalScenarios() {
+            return totalScenarios;
+        }
+
+        public String getPassingPercentage() {
+            return passingPercentage;
+        }
+
+        public List<Scenario> getAllScenarios() {
+            return allScenarios;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    @JacksonXmlRootElement(localName = "testsuite")
+    static class TestSuite {
+        @JacksonXmlElementWrapper(localName = "testcase", useWrapping = false)
+        private final List<Scenario> testcase;
+
+        @JsonCreator
+        public TestSuite(@JacksonXmlProperty(localName = "testcase") List<Scenario> testcase) {
+            this.testcase = testcase;
+        }
+    }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    static class Failure {
+    }
+
+    @SuppressWarnings("unused")
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Scenario {
+        private static Pattern pattern = Pattern.compile("Feature \"(.*?)\": Scenario \"(.*?)\"");
+
+        private final String name;
+        private final String featureName;
+        private final boolean failed;
+
+        public Scenario(String featureName, String name, boolean failed) {
+            this.name = name;
+            this.featureName = featureName;
+            this.failed = failed;
+        }
+
+        @JsonCreator
+        public static Scenario create(@JsonProperty("name") String title, @JsonProperty("failure") Failure failure) {
+            Matcher matcher = pattern.matcher(title);
+            checkState(matcher.find(), "Expecting string in format `Feature [name]: Scenario: [name]`, got `%s`", title);
+            String featureName = matcher.group(1);
+            String name = matcher.group(2);
+            return new Scenario(featureName, name, failure != null);
+        }
+
+        public boolean isFailed() {
+            return failed;
+        }
+
+        public boolean isPassed() {
+            return !failed;
+        }
+
+        public String getStatus() {
+            return isPassed() ? "Passed" : "Failed";
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getFeatureName() {
+            return featureName;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Scenario scenario = (Scenario) o;
+            return Objects.equals(featureName, scenario.featureName) &&
+                Objects.equals(name, scenario.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return name != null ? name.hashCode() : 0;
+        }
+    }
+}
---
+++ tools/tck-regression/src/test/java/org/opencypher/tools/tck/regression/TckResultsComparatorTest.java
@@ -0,0 +1,255 @@
+/*
+ * Copyright (c) 2015-2019 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Attribution Notice under the terms of the Apache License 2.0
+ *
+ * This work was created by the collective efforts of the openCypher community.
+ * Without limiting the terms of Section 6, any Derivative Work that is not
+ * approved by the public consensus process of the openCypher Implementers Group
+ * should not be described as “Cypher” (and Cypher® is a registered trademark of
+ * Neo4j Inc.) or as "openCypher". Extensions by implementers or prototypes or
+ * proposals for change that have been documented or implemented should only be
+ * described as "implementation extensions to Cypher" or as "proposed changes to
+ * Cypher that are not yet approved by the openCypher community".
+ */
+package org.opencypher.tools.tck.regression;
+
+import static java.lang.String.format;
+import static java.nio.file.Files.readAllBytes;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.UUID;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class TckResultsComparatorTest {
+    private static String test0;
+    private static String test1;
+    private static String test2;
+    private static String test3;
+    private static String test4;
+
+    @ClassRule
+    public static TemporaryFolder temp = new TemporaryFolder();
+
+    @BeforeClass
+    public static void setUp() throws Exception {
+        test0 = write(
+            "feature,scenario,status,reason\n" +
+                "Feature1,Scenario1,passed,n/a\n" +
+                "Feature1,Scenario2,passed,n/a\n" +
+                "Feature2,Scenario3,passed,n/a",
+            ".csv"
+        );
+
+        test1 = write(
+            "feature,scenario,status\n" +
+                "Feature1,Scenario1,failed\n" +
+                "Feature1,Scenario2,passed\n" +
+                "Feature2,Scenario3,passed\n",
+            ".csv"
+        );
+
+        test2 = write(
+            "feature,scenario,status,reason\n" +
+                "Feature1,Scenario1,failed,n/a\n" +
+                "Feature1,Scenario2,failed,n/a\n",
+            ".csv"
+        );
+
+        test3 = write(
+            "feature,scenario,status,reason\n" +
+                "Feature1,Scenario1,failed,n/a\n" +
+                "Feature1,Scenario2,failed,n/a\n" +
+                "Feature2,Scenario3,failed,n/a",
+            ".csv"
+        );
+
+        test4 = write(
+            "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+                "<testsuite name=\"org.opencypher.gremlin.tck.TckTest\" tests=\"958\" skipped=\"0\" failures=\"80\" errors=\"0\" timestamp=\"2019-05-15T11:38:52\" hostname=\"blkbox\" time=\"84.597\">\n" +
+                "  <properties/>\n" +
+                "  <testcase name=\"Feature &quot;Feature1&quot;: Scenario &quot;Scenario1&quot;\" classname=\"org.opencypher.tools.tck.TckTest\" time=\"5.638\">\n" +
+                "     <failure message=\"org.opencypher.tools.tck.api.Scenario$ScenarioFailedException:\"/>\n" +
+                "  </testcase>  \n" +
+                "  <testcase name=\"Feature &quot;Feature1&quot;: Scenario &quot;Scenario2&quot;\" classname=\"org.opencypher.tools.tck.TckTest\" time=\"5.638\"/>\n" +
+                "  <testcase name=\"Feature &quot;Feature2&quot;: Scenario &quot;Scenario3&quot;\" classname=\"org.opencypher.tools.tck.TckTest\" time=\"5.638\"/>\n" +
+                "</testsuite>\n",
+            ".xml"
+        );
+    }
+
+    @Test
+    public void testCompareImprove() throws Exception {
+        String path = temp.newFolder().getAbsolutePath();
+
+        comparator(test0, "--compareTo", test1, "--output", path);
+        String content = read(path, "regression.html");
+
+        assertThat(content, containsString("TCK coverage improved"));
+        assertThat(content, containsString(passing(1)));
+    }
+
+    @Test
+    public void testCompareNoChange() throws Exception {
+        String path = temp.newFolder().getAbsolutePath();
+
+        comparator(test0, "--compareTo", test0, "--output", path);
+        String content = read(path, "regression.html");
+
+        assertThat(content, containsString("Nothing changed"));
+    }
+
+    @Test
+    public void testCompareDegrade() throws Exception {
+        String path = temp.newFolder().getAbsolutePath();
+
+        assertThat(comparatorException(test1, "--compareTo", test0, "--output", path),
+            containsString("has failures"));
+
+        String content = read(path, "regression.html");
+
+        assertThat(content, containsString("TCK coverage degraded"));
+        assertThat(content, containsString(failing(1)));
+    }
+
+    @Test
+    public void testCompareXml() throws Exception {
+        String path = temp.newFolder().getAbsolutePath();
+
+        assertThat(comparatorException(test4, "--compareTo", test0, "--output", path),
+            containsString("has failures"));
+
+        String content = read(path, "regression.html");
+
+        assertThat(content, containsString("TCK coverage degraded"));
+        assertThat(content, containsString(failing(1)));
+    }
+
+    @Test
+    public void testDiffSize() throws Exception {
+        String path = temp.newFolder().getAbsolutePath();
+
+        assertThat(comparatorException(test1, "--compareTo", test2, "--output", path),
+            containsString("size changed"));
+
+        assertThat(comparatorException(test2, "--compareTo", test1, "--output", path),
+            containsString("size changed"));
+
+        assertThat(new File(path, "regression.html").exists(), is(false));
+    }
+
+
+    @Test
+    public void testVerifyOnlyFailed() throws Exception {
+        String path = temp.newFolder().getAbsolutePath();
+
+        assertThat(comparatorException(test0, "--verifyFailures", test1, "--output", path),
+            containsString("only failed scenarios"));
+
+        assertThat(new File(path, "regression.html").exists(), is(false));
+    }
+
+    @Test
+    public void testVerifyImprove() throws Exception {
+        String path = temp.newFolder().getAbsolutePath();
+
+        assertThat(comparatorException(test0, "--verifyFailures", test2, "--output", path),
+            containsString("does not match"));
+
+        String content = read(path, "regression.html");
+
+        assertThat(content, containsString(passing(2)));
+    }
+
+    @Test
+    public void testVerifyNoChange() throws Exception {
+        String path = temp.newFolder().getAbsolutePath();
+
+        comparator(test2, "--verifyFailures", test2, "--output", path);
+
+        String content = read(path, "regression.html");
+
+        assertThat(content, containsString("Nothing changed"));
+    }
+
+    @Test
+    public void testVerifyDegrade() throws Exception {
+        String path = temp.newFolder().getAbsolutePath();
+
+        assertThat(comparatorException(test3, "--verifyFailures", test2, "--output", path),
+            containsString("does not match"));
+
+        String content = read(path, "regression.html");
+
+        assertThat(content, containsString(failing(1)));
+    }
+
+    @Test
+    public void testConvertCsv() throws Exception {
+        String path = temp.newFolder().getAbsolutePath();
+
+        comparator(test4, "--saveCsv", "--output", path);
+
+        String actual = read(path, "base.csv").replaceAll("\r\n", "\n");
+
+        String expected = read("", test1);
+
+        assertThat(actual, equalTo(expected));
+    }
+
+
+    private void comparator(String... args) throws Exception {
+        TckResultsComparator.main(args);
+    }
+
+    private String comparatorException(String... args) {
+        try {
+            comparator(args);
+            return null;
+        } catch (Throwable t) {
+            return t.getMessage();
+        }
+    }
+
+    private String passing(int i) {
+        return format("Newly passing scenarios: <span class=\"label label-success\">%s</span>", i);
+    }
+
+    private String failing(int i) {
+        return format("Newly failed scenarios: <span class=\"label label-danger\">%s</span>", i);
+    }
+
+
+    private static String write(String content, String extension) throws Exception {
+        File file = temp.newFile(UUID.randomUUID() + extension);
+        Files.write(file.toPath(), content.getBytes(), StandardOpenOption.CREATE);
+        return file.getAbsolutePath();
+    }
+
+    private String read(String path, String file) throws IOException {
+        return new String(readAllBytes(new File(path, file).toPath()));
+    }
+}
