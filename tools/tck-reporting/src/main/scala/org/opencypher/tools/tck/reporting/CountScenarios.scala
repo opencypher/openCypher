8585b205806354736e45dd393d51622dc42fb854
hvub <hannes.voigt@neo4j.com>
2020-01-07 11:48:42 +0100

    add license header and a number of small code comments

--- tools/tck-reporting/src/main/scala/org/opencypher/tools/tck/reporting/CountScenarios.scala
+++ tools/tck-reporting/src/main/scala/org/opencypher/tools/tck/reporting/CountScenarios.scala
@@ -1,3 +1,30 @@
+/*
+ * Copyright (c) 2015-2020 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Attribution Notice under the terms of the Apache License 2.0
+ *
+ * This work was created by the collective efforts of the openCypher community.
+ * Without limiting the terms of Section 6, any Derivative Work that is not
+ * approved by the public consensus process of the openCypher Implementers Group
+ * should not be described as “Cypher” (and Cypher® is a registered trademark of
+ * Neo4j Inc.) or as "openCypher". Extensions by implementers or prototypes or
+ * proposals for change that have been documented or implemented should only be
+ * described as "implementation extensions to Cypher" or as "proposed changes to
+ * Cypher that are not yet approved by the openCypher community".
+ */
 package org.opencypher.tools.tck.reporting
 
 import org.opencypher.tools.tck.api.CypherTCK
@@ -13,10 +40,17 @@ case class Tag(name: String) extends CountCategory
 
 case class Feature(name: String) extends CountCategory
 
+/*
+ * This is a tiny tool to count TCK scenarios in the list returned by `CypherTCK.allTckScenarios`.
+ * At the moment it count scenarios by total, feature, and tags and outputs the counts to stdout.
+ * Run object `CountScenarios` to run the tool.
+ */
 case object CountScenarios {
   def main(args: Array[String]): Unit = {
     val scenarios = CypherTCK.allTckScenarios
+    // create individual counts to each scenario
     val individualCounts = scenarios.map(s => {
+        // total
         val totalMap = Map[CountCategory,Int](Total -> 1)
         // feature
         val featureMap = Map[CountCategory,Int](Feature(s.featureName) -> 1)
@@ -25,7 +59,7 @@ case object CountScenarios {
 
         totalMap ++ featureMap ++ tagsMap
       })
-
+    // total up the counts over all scenarios
     val totalCounts = individualCounts.foldLeft(Map[CountCategory,Int]()){
       case (cnt, cnts) => {
         (cnts.keySet ++ cnt.keySet).map {
@@ -33,7 +67,7 @@ case object CountScenarios {
         }.toMap
       }
     }
-
+    // print counts to stdout
     println(totalCounts.map{ case (cat, count) => "" + cat + "\t" + count}.mkString(System.lineSeparator))
   }
 }
