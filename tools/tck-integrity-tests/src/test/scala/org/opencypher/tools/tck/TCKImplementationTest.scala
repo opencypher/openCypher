8914356df9a6d3c616cfcf3f2d59a32fe84ebc94
Hannes Voigt <30618026+hvub@users.noreply.github.com>
2020-10-07 16:51:07 +0200

    Add test to run TCK on Neo4j

--- tools/tck-integrity-tests/src/test/scala/org/opencypher/tools/tck/TCKImplementationTest.scala
+++ tools/tck-integrity-tests/src/test/scala/org/opencypher/tools/tck/TCKImplementationTest.scala
@@ -1,13 +1,38 @@
+/*
+ * Copyright (c) 2015-2020 "Neo Technology,"
+ * Network Engine for Objects in Lund AB [http://neotechnology.com]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Attribution Notice under the terms of the Apache License 2.0
+ *
+ * This work was created by the collective efforts of the openCypher community.
+ * Without limiting the terms of Section 6, any Derivative Work that is not
+ * approved by the public consensus process of the openCypher Implementers Group
+ * should not be described as “Cypher” (and Cypher® is a registered trademark of
+ * Neo4j Inc.) or as "openCypher". Extensions by implementers or prototypes or
+ * proposals for change that have been documented or implemented should only be
+ * described as "implementation extensions to Cypher" or as "proposed changes to
+ * Cypher that are not yet approved by the openCypher community".
+ */
 package org.opencypher.tools.tck
 
 import java.util
 
 import cypher.features.InterpretedTCKTests
-import org.junit.jupiter.api.DynamicTest
-import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.{Disabled, DynamicTest, TestFactory}
 
 class TCKImplementationTest {
-  @TestFactory
-  def runNeo4j(): util.Collection[DynamicTest] =
-    (new InterpretedTCKTests()).runInterpreted()
+  @TestFactory @Disabled
+  def runTckOnNeo4j(): util.Collection[DynamicTest] = new InterpretedTCKTests().runInterpreted()
 }
