b3df539f30d885e28337ea709eeccae83fe85493
Hannes Voigt <30618026+hvub@users.noreply.github.com>
2021-01-08 19:05:58 +0100

    Add new ScenarioFormatChecker

--- tools/tck-integrity-tests/src/main/scala/org/opencypher/tools/tck/ScenarioFormatChecker.scala
+++ tools/tck-integrity-tests/src/main/scala/org/opencypher/tools/tck/ScenarioFormatChecker.scala
@@ -27,21 +27,41 @@
  */
 package org.opencypher.tools.tck
 
-import org.opencypher.tools.grammar.Antlr4TestUtils
+import org.opencypher.tools.tck.api.Scenario
+import org.opencypher.tools.tck.api.groups.ContainerGroup
+import org.opencypher.tools.tck.api.groups.Group
+import org.opencypher.tools.tck.api.groups.Item
+import org.opencypher.tools.tck.api.groups.Tag
+import org.opencypher.tools.tck.api.groups.TckTree
+import org.opencypher.tools.tck.api.groups.Total
+import org.scalatest.OptionValues
+import org.scalatest.funspec.AnyFunSpecLike
+import org.scalatest.matchers.should.Matchers
 
-class validateQueryGrammarTest extends TckTestSupport {
+import scala.language.implicitConversions
 
-  validateQueryGrammar.f = new java.util.function.Consumer[String] {
-    override def accept(query: String): Unit = Antlr4TestUtils.parse(query)
-  }
+trait ScenarioFormatChecker extends AnyFunSpecLike with Matchers with OptionValues with ValidateScenario {
 
-  test("parsing should work") {
-    validateQueryGrammar("MATCH (a) RETURN a")
-  }
+  def create(scenarios: Seq[Scenario]): Unit = {
 
-  test("bad query should fail parsing") {
-    an [AssertionError] shouldBe thrownBy {
-      validateQueryGrammar("not a query")
+    implicit val tck: TckTree = TckTree(scenarios)
+
+    def spawnTests(currentGroup: Group): Unit = {
+      currentGroup match {
+        case Total =>
+          Total.children.foreach(spawnTests)
+        case g: ContainerGroup =>
+          describe(g.description) {
+            g.children.foreach(spawnTests)
+          }
+        case i: Item =>
+          describe(i.description) {
+            validateScenario(i.scenario)
+          }
+        case _ => Unit
+      }
     }
+
+    spawnTests(Total)
   }
 }
